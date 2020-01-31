2d1656c09addf9928ce16c345ced7e398b1a30a2
YoanaStoyanova <yy.stoyanova@abv.bg>
2020-01-31 11:48:47 -0400

    Initial angular project setup

--- boaty-app/src/test/java/ApplicationTest.java
+++
@@ -1,7 +0,0 @@
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
-public class ApplicationTest {
-
-}
--- boaty-app/src/test/java/org/fmi/spring/boatyapp/ApplicationTest.java
+++
@@ -1,7 +0,0 @@
-package org.fmi.spring.boatyapp;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ApplicationTest {
-
-}
